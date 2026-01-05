587cace10a7806ff814b9ae64ee51f61fa55a9ac
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-01-05 17:56:33 +0000

    added logo pictures and updated index.js to test connectivity

---
+++ index.js
@@ -0,0 +1,18 @@
+// test index.js for basic connectivity
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
